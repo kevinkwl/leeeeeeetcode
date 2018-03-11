a2bb8679bd281be058e23e4118eec49751fbccc6
kevinkwl <lingkangwei.kevin@gmail.com>
2018-03-11 17:13:02 +0800

    minor change to algo 42

--- algorithms/42-Trapping-Rain-Water.c
+++ algorithms/42-Trapping-Rain-Water.c
@@ -4,18 +4,17 @@
 #define min(a, b) ((a) < (b) ? (a) : (b))
 
 int trap(int* height, int heightSize) {
-    int water = 0, level;
+    int water = 0;
     int lmax, rmax, l = 0, r = heightSize - 1;
     lmax = rmax = 0;
     while (l < r) {
-        level = min(lmax, rmax);
         lmax = max(lmax, height[l]);
         rmax = max(rmax, height[r]);
         if (height[l] < height[r]) {
-            water += max(0, (level - height[l]));
+            water += max(0, min(lmax, rmax) - height[l]);
             l++;
         } else {
-            water += max(0, (level - height[r]));
+            water += max(0, min(lmax, rmax) - height[r]);
             r--;
         }
     }
